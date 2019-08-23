91a949291ad38af62e68f445542d99a253322b6f
Youngsung Kim <grnydawn@gmail.com>
2019-08-23 15:41:29 -0600

    version 0.1.1

--- earthplot.py
+++ earthplot.py
@@ -13,7 +13,7 @@ Examples
 ---------
 """
     _name_ = "earthplot"
-    _version_ = "0.1.0"
+    _version_ = "0.1.1"
     _install_requires_ = ["ncplot", "cartopy"]
 
     def __init__(self, parent):
